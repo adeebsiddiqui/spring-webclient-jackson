ca11d590fb7412fbd47bf4cf3487677ce37d1839
Adeeb Siddiqui <adeeb.bd@gmail.com>
2021-08-16 03:44:10 -0500

    Adding initial source code from spring initializr

---
+++ src/main/java/com/ad/springwebclientjackson/SpringWebclientJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.ad.springwebclientjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebclientJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebclientJacksonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad/springwebclientjackson/SpringWebclientJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.springwebclientjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebclientJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
